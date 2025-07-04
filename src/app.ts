60b5eb6767621932a7b42b76dd09b9a3c7b1374b
tientran1234 <tranngoctien29112003@gmail.com>
2025-07-04 19:36:05 +0700

    change port

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import { AppError } from './handlers/error';
 
 
 const CONFIG = {
-    TCP_PORT: process.env.ADMIN_POD_TCP_PORT || '4003',
+    TCP_PORT: process.env.ADMIN_POD_TCP_PORT || 4003,
     TCP_HOST: process.env.ADMIN_POD_HOST || '0.0.0.0',
     MAX_CONNECTIONS: parseInt(process.env.MAX_TCP_CONNECTIONS || '100', 10),
     SOCKET_TIMEOUT: parseInt(process.env.SOCKET_TIMEOUT || '30000', 10), // 30s
